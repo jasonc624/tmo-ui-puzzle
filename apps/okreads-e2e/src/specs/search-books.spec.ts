94b9222baa84f512bd8bfe91385f82dbe8b873e9
Jason Cardenas <jasonc@rhythmic-rebellion.com>
2020-08-17 17:03:45 -0400

    implemented fast search

--- apps/okreads-e2e/src/specs/search-books.spec.ts
+++ apps/okreads-e2e/src/specs/search-books.spec.ts
@@ -1,5 +1,8 @@
 import { $, $$, browser, ExpectedConditions } from 'protractor';
 import { expect } from 'chai';
+import { BookSearchComponent } from '../../../../libs/books/feature/src/lib/book-search/book-search.component';
+import { TestBed, tick } from '@angular/core/testing';
+import { SinonSpy } from 'sinon';
 
 describe('When: Use the search feature', () => {
   it('Then: I should be able to search books by title', async () => {
@@ -17,12 +20,21 @@ describe('When: Use the search feature', () => {
     expect(items.length).to.be.greaterThan(1, 'At least one book');
   });
 
-  xit('Then: I should see search results as I am typing', async () => {
+  it('Then: I should see search results as I am typing', async () => {
+
     await browser.get('/');
     await browser.wait(
       ExpectedConditions.textToBePresentInElement($('tmo-root'), 'okreads')
     );
-
     // TODO: Implement this test!
+    const fixture = TestBed.createComponent(BookSearchComponent);
+    fixture.detectChanges();
+    const grid = await $('book-grid');
+    const gridItems = await $$('[data-testing="book-item"]');
+    expect(grid).to.be.empty;
+    const input = await $('input[type="search"]');
+    await input.sendKeys('javascript');
+    tick(500);
+    expect(gridItems.length).to.be.greaterThan(1, 'There are books');
   });
 });
--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
   addToReadingList,
@@ -9,16 +9,17 @@ import {
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'tmo-book-search',
   templateUrl: './book-search.component.html',
   styleUrls: ['./book-search.component.scss']
 })
-export class BookSearchComponent implements OnInit {
-  books: Observable<ReadingListBook[]>= this.store.select(getAllBooks);
-
+export class BookSearchComponent implements OnInit, OnDestroy {
+  books: Observable<ReadingListBook[]> = this.store.select(getAllBooks);
+  ngUnsubscribe = new Subject();
   searchForm = this.fb.group({
     term: ''
   });
@@ -26,13 +27,18 @@ export class BookSearchComponent implements OnInit {
   constructor(
     private readonly store: Store,
     private readonly fb: FormBuilder
-  ) {}
+  ) {
+  }
 
   get searchTerm(): string {
     return this.searchForm.value.term;
   }
 
   ngOnInit(): void {
+    this.searchForm.get('term').valueChanges.pipe(
+      debounceTime(500),
+      takeUntil(this.ngUnsubscribe)
+    ).subscribe(() => this.searchBooks());
   }
 
   formatDate(date: void | string) {
@@ -51,10 +57,12 @@ export class BookSearchComponent implements OnInit {
   }
 
   searchBooks() {
-    if (this.searchForm.value.term) {
-      this.store.dispatch(searchBooks({ term: this.searchTerm }));
-    } else {
+    this.searchForm.value.term ? this.store.dispatch(searchBooks({ term: this.searchTerm })) :
       this.store.dispatch(clearSearch());
-    }
+  }
+
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
